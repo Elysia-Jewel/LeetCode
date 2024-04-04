95c83c6d60a03c0a192c44d8631cab980876eeb1
elee0081 <elee0081@student.monash.edu>
2024-04-04 16:33:27 +0800

    new methods of solving

--- 3.py
+++ 3.py
@@ -11,38 +11,6 @@ def twoSum1(nums: list[int], target: int) -> list[int]:
                 return [i, j]
 
 def twoSum2(nums: list[int], target: int) -> list[int]:
-    #sort
-    sorted_nums = sorted(nums)
-    mid = sorted_nums[len(sorted_nums) // 2]
-    if target - mid:
-        pass
-    #get num at midpoint
-    #maybe use recursion? divide and conquer?
-    return
-
-def twoSum3(nums: list[int], target: int) -> list[int]:
-    def binary_search(lst, key):
-        lo = 0
-        hi = len(lst) - 1
-        mid = (len(lst) - 1) // 2
-        while lo <= hi:
-            mid = (hi + lo) // 2
-            if key == lst[mid]:
-                return mid
-            elif key > lst[mid]:
-                lo = mid + 1
-            else:
-                hi = mid - 1
-        return None
-
-    sorted_nums = sorted(nums)
-    for i in range(len(nums)):
-        key = target - sorted_nums[i]
-        res = binary_search(sorted_nums[i + 1:], key)
-        if res != None:
-            return [i, res+i+1]
-
-def twoSum4(nums: list[int], target: int) -> list[int]:
     table = {}
     for i in range(len(nums)):
         table[nums[i]] = i
@@ -52,7 +20,36 @@ def twoSum4(nums: list[int], target: int) -> list[int]:
         j = table.get(key)
         if j != None and j != i:
             return [i, j]
+
+def twoSum3(nums: list[int], target: int) -> list[int]:
+    table = {}
+    for i, num in enumerate(nums):
+        diff = target - num
+        if diff in table:
+            return [table[diff], i]
+        table[num] = i
+
+def twoSum4(nums: list[int], target: int) -> list[int]:
+    table = {}
+    for num in nums:
+        diff = target - num
+        if diff in table:
+            return [table[diff], nums.index(num)] 
+            #does not work because index() returns the index of the first occurence of the element, not the index of the current occurence of the element
+        table[num] = nums.index(num)
+
+def twoSum4fixed(nums: list[int], target: int) -> list[int]:
+    table = {}
+    for i in range(len(nums)):
+        diff = target - nums[i]
+        if diff in table:
+            return [table[diff], i]
+        else:
+            table[nums[i]] = i
+
+
 lst1 = [2,7,11,15]
 lst2 = [1, 2, 3, 4, 5, 6, 7, 8]
 lst3 = [3,2,4]
-print(twoSum4(lst3, 6))
+lst4 = [3, 3]
+print(twoSum4fixed(lst4, 6))
